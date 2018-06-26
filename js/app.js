8338428c89aa9ce089f17a2e8165656ee6ab7a10
kinwail <kinwail@gmail.com>
2018-06-25 19:01:48 -1000

    completed #8

--- js/app.js
+++ js/app.js
@@ -75,9 +75,9 @@ function showPrice(){
 
 var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
-displayQuote.addEventListener('click', quoteDisplay);
+Benjamin.addEventListener('click', quoteBox);
 
-function quoteDisplay(){
+function quoteBox(){
     displayQuote.innerHTML = myQuote;
 }
 
@@ -87,8 +87,14 @@ function quoteDisplay(){
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+random.addEventListener('click', randomQuotes);
 
+var x = Math.floor((Math.random() * quotes.length))
+function randomQuotes(){
+    displayQuotes.innerHTML = quotes[x];
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+
